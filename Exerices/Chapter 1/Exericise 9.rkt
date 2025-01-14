53e3b1d59af7dac971d37bbad786ac39a8b56bb6
Anish Lukkireddy <57578712+anishlukk123@users.noreply.github.com>
2025-01-14 17:07:22 -0500

    Add files via upload

---
+++ Exerices/Chapter 1/Exericise 9.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Exericise 9|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define in  -20)
+ 
+(if (string? in) (string-length in)  " not a string" )
+
+(if (number? in)    (abs in)  " not a number"  )
+
+
+
+
