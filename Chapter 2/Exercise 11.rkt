263cac169c1a1cc4eff2208f7da46289fdfca1a9
Anish Lukkireddy <57578712+anishlukk123@users.noreply.github.com>
2025-03-24 21:06:05 -0400

    Add files via upload

---
+++ Chapter 2/Exercise 11.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Exercise 11|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;given two numbers x and y return the distance from the orgin
+ ; number ->  number
+
+ ; (3,4) -> 5
+  ;(2,0) -> 2
+  ;(0,0) -> 0 
+
+(define  (calculate_volume_cube x  ) (sqrt( + (sqr x) (sqr y))))
+
+   
+
+
+(distance_from_orgin 3 4)
